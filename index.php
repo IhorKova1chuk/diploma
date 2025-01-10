d73470ef8360d96cacbf2aa4bceec62d197991ae
IhorKova1chuk <kovalhyk.ihor26@gmail.com>
2025-01-11 01:13:19 +0200

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-echo 48;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="full_width">
+    <div class="container">
+        <header>
+            <div class="logo">My logo</div>
+            <div class="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </header>
+        <main></main>
+    </div>
+</div>
+
+</body>
+</html>
